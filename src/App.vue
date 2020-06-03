fdbaef5495ab547870c5e5a3d29d760ae1029619
marcsanuy <marcsanuy@gmail.com>
2020-06-03 20:12:32 +0200

    Componet Cart finished

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app" class="mt-5">
-   <shop-layout>
+    <shop-layout>
       <template slot="product-list">
         <product-list></product-list>
       </template>
       <template slot="cart">
-        Carrito
-      </template>  
-   </shop-layout>
+        <cart></cart>
+      </template>
+    </shop-layout>
   </div>
 </template>
-<templa
 
 <script>
 
 import ShopLayout from '@/components/ShopLayout'
 import ProductList from '@/components/ProductList'
+import Cart from '@/components/Cart'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    ShopLayout, ProductList
+    ShopLayout, ProductList, Cart
   }
 }
 </script>
